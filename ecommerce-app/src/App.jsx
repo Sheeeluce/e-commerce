f8fa0f3f10133e49581ee24d2c7c9497f828138d
Lucy <lucymwago9@gmail.com>
2026-05-20 23:46:04 +0300

    routing

--- ecommerce-app/src/App.jsx
+++ ecommerce-app/src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Product from './pages/Product'
+import AddProduct from './pages/AddProduct'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   
 
   return (
-    <>
+    <div>
       
-    </>
+      <Navbar/>
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/product' element={<Product/>}/>
+        <Route path='/add-product' element={<AddProduct/>}/>
+
+      </Routes>
+    </div>
   )
 }
 
