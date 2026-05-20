ab24f9e899cb7b863a2d9b18e1d8acced3d6b571
Lucy <lucymwago9@gmail.com>
2026-05-21 02:22:45 +0300

    cake home

--- ecommerce-app/src/pages/Home.jsx
+++ ecommerce-app/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 export default function Home() {
     return (
         <div>
-            <h1>Admin Dashboard</h1>
-            <p>Manage e-commerce products.</p>
+            <h1>Cake Canvas</h1>
+            <p>Welcome to Cake Canvas!</p>
+            <img className="cake" src="https://www.pngfind.com/pngs/m/32-326625_cake-cake-icon-transparent-background-hd-png-download.png" alt="cake" />
         </div>
     )
 }
